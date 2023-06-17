085569ccdd7d3f1e586854f269ad232ec757658a
chris <chr33s@icloud.com>
2023-06-17 13:15:12 +0700

    fix: api/proxy/graphql/storefront bug

--- functions/api/proxy/graphql/storefront.ts
+++ functions/api/proxy/graphql/storefront.ts
@@ -5,14 +5,22 @@ export const onRequestPost: PagesFunction<Env> = async (context) => {
 	const session: any = await getSession(context, true);
 
 	const admin = new (shopify(context).clients.Rest)({ session });
-	const storefrontTokens: any = await admin.get({
-		path: "storefront_access_tokens",
-	});
-	const storefrontAccessToken =
-		storefrontTokens.body.storefront_access_tokens.find(
-			(v: any) => v.title === config.storefrontAccessTokenTitle
-		)?.access_token;
-	if (storefrontAccessToken) {
+	const storefrontAccessToken = await admin
+		.get<{
+			storefront_access_tokens: {
+				access_token: string;
+				title: string;
+			}[];
+		}>({
+			path: "storefront_access_tokens",
+		})
+		.then(
+			(res) =>
+				res.body.storefront_access_tokens.find(
+					(v) => v.title === config.storefrontAccessTokenTitle
+				)?.access_token
+		);
+	if (!storefrontAccessToken) {
 		return new Response("No storefront token found", { status: 403 });
 	}
 
