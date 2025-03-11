3b899d11f007d235cac0f23b708e17e55d0e82ca
chris <chr33s@icloud.com>
2025-03-11 14:08:03 +0700

    update: fix proxy test skip

--- app/routes/proxy.worker.test.ts
+++ app/routes/proxy.worker.test.ts
@@ -61,21 +61,46 @@ describe("loader", () => {
 		expect(await response?.text()).toBe("Invalid hmac");
 	});
 
-	test.skip("success", async () => {
-		const url = new URL("http://localhost");
-		url.searchParams.set(
-			"signature",
-			"548e324a5420c20bffa1d81318b5790de43731c278d0435108e5bcdbdc20795c",
+	test("success", async () => {
+		const searchParams = {
+			timestamp: Math.trunc(Date.now() / 1_000).toString(),
+		};
+		const params = Object.entries(searchParams)
+			.filter(([key]) => key !== "signature")
+			.sort(([a], [b]) => a.localeCompare(b))
+			.reduce((acc, [key, value]) => {
+				return `${acc}${key}=${Array.isArray(value) ? value.join(",") : value}`;
+			}, "");
+
+		const encoder = new TextEncoder();
+		const encodedKey = encoder.encode(
+			context.cloudflare.env.SHOPIFY_API_SECRET_KEY,
 		);
-		url.searchParams.set("timestamp", `${Math.trunc(Date.now() / 1_000)}`);
-		const request = new Request(url, { method: "POST" });
+		const encodedData = encoder.encode(params);
+		const hmacKey = await crypto.subtle.importKey(
+			"raw",
+			encodedKey,
+			{
+				name: "HMAC",
+				hash: "SHA-256",
+			},
+			true,
+			["sign"],
+		);
+		const signature = await crypto.subtle.sign("HMAC", hmacKey, encodedData);
+		const hmac = [...new Uint8Array(signature)].reduce(
+			(a, b) => a + b.toString(16).padStart(2, "0"),
+			"",
+		); // hex
+
+		const url = new URL("http://localhost");
+		Object.entries(searchParams).forEach(([key, value]) => {
+			url.searchParams.set(key, value);
+		});
+		url.searchParams.set("signature", hmac);
+		const request = new Request(url, { body: "{}", method: "POST" });
 		const response = await loader({ context, request } as Route.LoaderArgs);
 
-		console.log(await response.text());
-
-		expect(response).toBeInstanceOf(Response);
-		expect(response?.ok).toBe(true);
-		expect(response?.status).toBe(204);
-		expect(response?.body).toBe(null);
+		expect(response).toBe(undefined);
 	});
 });
