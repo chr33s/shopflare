3ec21b42b3f62a355e3f01d881c66fc07c32b1fb
chris <chr33s@icloud.com>
2025-03-24 14:57:36 +0700

    update: react-router.config#future

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
 	// Config options...
 	future: {
+		unstable_optimizeDeps: true,
+		unstable_splitRouteModules: true,
 		unstable_viteEnvironmentApi: true,
 	},
 	// Server-side render by default, to enable SPA mode set this to `false`
