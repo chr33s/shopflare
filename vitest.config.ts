73548287c8951d37e292e70f1a287a9e1e879737
chris <chr33s@icloud.com>
2025-04-24 09:18:50 +0700

    update: vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,9 @@ export default defineConfig((config) => {
 	const env = loadEnv(config.mode, process.cwd(), "");
 
 	return {
+		optimizeDeps: {
+			include: ["react/jsx-dev-runtime"],
+		},
 		plugins: [tsconfigPaths()],
 		test: {
 			css: true,
