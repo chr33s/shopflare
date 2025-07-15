3d25c67dd79c12610da744e4401000a640ffd0a7
chris <chr33s@icloud.com>
2025-07-15 13:18:52 +0700

    update: types

--- app/types/app.types.d.ts
+++ app/types/app.types.d.ts
@@ -1,27 +1,17 @@
-import type { FunctionComponent, SVGAttributes } from "react";
-
-declare module "*.css" {
-	const content: string;
-	export default content;
-}
+import type { UIModalAttributes, UITitleBarAttributes, UISaveBarAttributes, UINavMenuAttributes } from "@shopify/app-bridge-types";
 
 declare module "*.json" {
 	const content: string;
 	export default content;
 }
 
-declare module "*.svg" {
-	const content: FunctionComponent<SVGAttributes<SVGElement>>;
-	export default content;
-}
-
 declare module "react" {
 	namespace JSX {
 		interface IntrinsicElements {
-			"ui-modal": React.JSX.IntrinsicElements.UIModalAttributes;
-			"ui-title-bar": React.JSX.IntrinsicElements.UITitleBarAttributes;
-			"ui-save-bar": React.JSX.IntrinsicElements.UISaveBarAttributes;
-			"ui-nav-menu": React.JSX.IntrinsicElements.UINavMenuAttributes;
+			"ui-modal": UIModalAttributes;
+			"ui-title-bar": UITitleBarAttributes;
+			"ui-save-bar": UISaveBarAttributes;
+			"ui-nav-menu": UINavMenuAttributes;
 		}
 	}
 }
