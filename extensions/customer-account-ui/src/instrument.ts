7039a1b2e8359f7f7a59f06d1747fdd88061e998
chris <chr33s@icloud.com>
2025-07-31 12:53:38 +0700

    update: sentry

--- extensions/customer-account-ui/src/instrument.ts
+++ extensions/customer-account-ui/src/instrument.ts
@@ -1,6 +1,14 @@
-import * as Sentry from "@sentry/react";
+import * as Sentry from "@sentry/browser";
 
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+Sentry.init({
+	defaultIntegrations: false,
+	// eslint-disable-next-line no-process-env
+	dsn: process.env.SENTRY_DSN,
+	// eslint-disable-next-line no-process-env
+	environment: process.env.ENVIRONMENT,
+	// eslint-disable-next-line no-process-env
+	release: process.env.npm_package_version ?? "0.0.0",
+});
 
 self.addEventListener("unhandledrejection", (error) => {
 	Sentry.captureException(new Error(error.reason.stack));
