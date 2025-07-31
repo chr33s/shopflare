9a6135eb93646bd54134b258661fef2e515cb16a
chris <chr33s@icloud.com>
2025-07-31 13:00:22 +0700

    add: vite import.meta.env types

--- shopflare.d.ts
+++ shopflare.d.ts
@@ -4,6 +4,7 @@ declare module 'cloudflare:test' {
 	// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-empty-object-type
 	interface ProvidedEnv extends Env {}
 }
+
 declare module 'react' {
 	// eslint-disable-next-line @typescript-eslint/no-namespace
 	export namespace JSX {
@@ -25,5 +26,21 @@ declare module 'react-router' {
 	}
 	export interface Future {
 		unstable_viteEnvironmentApi: true;
+	}
+}
+
+declare module 'vite/client' {
+	// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-empty-object-type
+	interface ViteTypeOptions {}
+
+	interface ImportMetaEnv {
+		readonly SHOPIFY_API_KEY: string;
+		readonly SHOPIFY_APP_HANDLE: string;
+		readonly SHOPIFY_APP_LOG_LEVEL: string;
+		readonly SHOPIFY_APP_URL: string;
+	}
+
+	export interface ImportMeta {
+		readonly env: ImportMetaEnv;
 	}
 }
