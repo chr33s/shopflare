7d5d5e87c91e1b31b2570b401ae5973ad97c96c6
chris <chr33s@icloud.com>
2025-08-26 15:24:14 +0700

    update: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,12 @@
+import { fileURLToPath } from "node:url";
+import {
+	defineWorkersConfig,
+	defineWorkersProject,
+} from "@cloudflare/vitest-pool-workers/config";
 import { loadEnv } from "vite";
 import i18nextLoader from "vite-plugin-i18next-loader";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 
 import i18nextLoaderOptions from "./i18n.config";
 
@@ -16,6 +21,61 @@ export default defineConfig((config) => {
 		test: {
 			css: true,
 			env,
+			projects: [
+				{
+					extends: "./vitest.config.ts",
+					test: {
+						browser: {
+							headless: true,
+							enabled: true,
+							instances: [{ browser: "webkit" }],
+							provider: "playwright",
+						},
+						include: ["app/**/*.browser.test.tsx"],
+						name: "browser",
+					},
+				},
+				{
+					extends: "./vitest.config.ts",
+					test: {
+						environment: "happy-dom",
+						include: ["app/**/*.client.test.tsx"],
+						name: "client",
+					},
+				},
+				defineWorkersConfig(
+					mergeConfig(
+						{ extends: "./vitest.config.ts" },
+						defineWorkersProject({
+							test: {
+								alias: [
+									{
+										find: "virtual:react-router/server-build",
+										replacement: fileURLToPath(
+											new URL("./build/server/index.js", import.meta.url),
+										),
+									},
+								],
+								include: ["worker.test.ts", "app/**/*.server.test.ts"],
+								name: "server",
+								poolOptions: {
+									workers: {
+										main: "./build/server/index.js",
+										miniflare: {
+											compatibilityFlags: [
+												"nodejs_compat",
+												"service_binding_extra_handlers",
+											],
+										},
+										singleWorker: true,
+										wrangler: { configPath: "./wrangler.json" },
+									},
+								},
+							},
+						}),
+					),
+				),
+			],
 			watch: false,
 		},
 	};
