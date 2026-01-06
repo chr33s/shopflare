b23706d9eb25495ecc7585b4e61ddab1877cb307
chris <chr33s@icloud.com>
2026-01-06 09:13:29 +0700

    update: packages

--- app/types/admin.generated.d.ts
+++ app/types/admin.generated.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-import type * as AdminTypes from './admin.types';
+import type * as AdminTypes from './admin.types.d.ts';
 
 export type ShopQueryVariables = AdminTypes.Exact<{ [key: string]: never; }>;
 
